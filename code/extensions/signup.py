9b82137119361a226e696f14e85c094ca5fd3a7a
Peter Kerr <>
2021-04-29 23:59:17 +0000

    Rename variable for clarity

--- code/extensions/signup.py
+++ code/extensions/signup.py
@@ -48,8 +48,8 @@ class Signup(commands.Cog):
 
                 numNewResponses = currNumResponses - self.numResponses
                 for newResponse in responses[-numNewResponses:]:
-                    secondsPerMonth = 60*60*24*30
-                    invite = await channel.create_invite(max_uses=1, max_age=secondsPerMonth, reason=f"Generating invite for {newResponse[self.realNameIndex]}")
+                    monthInSeconds = 60*60*24*30
+                    invite = await channel.create_invite(max_uses=1, max_age=monthInSeconds, reason=f"Generating invite for {newResponse[self.realNameIndex]}")
                     self.sendEmail(newResponse[self.emailAddrIndex], invite)
                 self.numResponses = currNumResponses
 
