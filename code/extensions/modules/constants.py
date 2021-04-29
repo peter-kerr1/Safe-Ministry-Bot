db617f5dd49a84e6f6b5466edae409b039071a2c
Peter Kerr <>
2021-04-29 07:05:17 +0000

    Changed Leader role string back to 'Leader'

--- code/extensions/modules/constants.py
+++ code/extensions/modules/constants.py
@@ -3,6 +3,6 @@ from enum import Enum
 class Roles(Enum):
     ADMIN = 'Admin'
     YOUTH_MINISTER = 'Youth Minister'
-    LEADER = 'Leaders'
+    LEADER = 'Leader'
     YOUTH = 'Youth'
     MUTED = 'Muted'
