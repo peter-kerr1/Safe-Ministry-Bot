2862a62eca43dfe4234135ef39c1de29202c8e2e
Peter Kerr <>
2021-05-01 04:05:52 +0000

    Added clarity to member join fail message

--- code/extensions/new_member.py
+++ code/extensions/new_member.py
@@ -28,7 +28,7 @@ class NewMember(commands.Cog, name='New Member'):
         if not responses:
             print("[ERROR]: Failed to retrieve data from Google Form responses!")
             return
-            
+
         accountName = f"{member.name}#{member.discriminator}"
         reponse = find(lambda r: r[self.accNameIndex] == accountName, responses[::-1])
         if reponse is not None:
@@ -36,9 +36,9 @@ class NewMember(commands.Cog, name='New Member'):
             await member.edit(nick=nickname)
         else:
             if await addRole(member, "Muted", f"{member.name}'s permission form could not be found, muting."):
-                await member.send("You have been muted because I can't find your permission form.\n"
+                await member.send("You have been muted because I can't find your permission form to join the St Matts Youth Discord group.\n"
                                     "This is probably because your Discord username was mispelt in the form - try filling it out again and rejoining the server.\n"
-                                    "Otherwise, you can message a member with the Admin role and they should be able to help you!")
+                                    "Otherwise, you can message a member with the @Admin role and they should be able to help you!")
 
 def setup(bot):
     bot.add_cog(NewMember(bot))
