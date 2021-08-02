8dfd6e42d845123588cd67803d3f9b726cddd340
Peter Kerr <30363159+peter-kerr1@users.noreply.github.com>
2021-08-02 07:02:04 +0000

    Make video mute more efficient and fix mute bug when bot present

--- code/extensions/safe_ministry.py
+++ code/extensions/safe_ministry.py
@@ -13,11 +13,12 @@ class SafeMinistry(commands.Cog, name='Safe Ministry'):
     """
     def __init__(self, bot):
         self.bot = bot
-        self.tempChannel = None
+
 
     # Returns True if there is a Youth member present in the list of members, False otherwise
     def youthPresent(self, members):
         return find(lambda m: hasRole(m, [Roles.YOUTH.value]), members) is not None
+
 
     # Returns False if there are youth members in the voice channel and there are less than 2 leaders present.
     # True otherwise (including if there are no youth members.)
@@ -29,6 +30,7 @@ class SafeMinistry(commands.Cog, name='Safe Ministry'):
                 return False
         return True
 
+
     # Deafens or undeafens all members in a voice channel, based on whether 'deaf' is True or False.
     # Sharing camera/screen is included in this definition.
     async def setChannelDeafness(self, voiceChannel, deaf):
@@ -38,33 +40,40 @@ class SafeMinistry(commands.Cog, name='Safe Ministry'):
         # Enable/disable sharing camera & screen
         await voiceChannel.set_permissions(voiceChannel.guild.default_role, stream=(not deaf))
 
-        # Manage deafness of members, shuffle if sharing camera/screen and deaf=True.
+        # Shuffle members if sharing camera/screen and deaf=True.
         # (Disabling sharing camera/screen doesn't stop them sharing if they were already doing so.)
+        muteVideo = []
+        if deaf is True:
+            muteVideo = [member for member in voiceChannel.members if member.voice.self_stream or member.voice.self_video]
+        if len(muteVideo) > 0:
+            tempChannel = await category.create_voice_channel(f"{voiceChannel.name}-temp-{voiceChannel.guild.id}")
+            for member in muteVideo:
+                await member.move_to(tempChannel)
+            await asyncio.sleep(0.5)
+            for member in muteVideo:
+                await member.move_to(voiceChannel)
+            await tempChannel.delete()
+
+        # Manage deafness of members
         for member in voiceChannel.members:
             await member.edit(deafen=deaf)
-            if deaf is True:
-                if member.voice.self_stream or member.voice.self_video:
-                    self.tempChannel = await category.create_voice_channel(f"{voiceChannel.name}-temp")
-                    await member.move_to(self.tempChannel)
-                    await asyncio.sleep(0.5)
-                    await member.move_to(voiceChannel)
-                    await self.tempChannel.delete()
-                if member.dm_channel is None:
+            if deaf is True and member.bot is False and member.dm_channel is None:
                     await member.send(f"**Voice channel currently disabled:** there are less than two leaders in the {voiceChannel.mention} voice channel.\n"
-                                       "The channel will be enabled again when two or more leaders join.\n"
-                                       "*This is the only time you will receive this message.*")
+                                    "The channel will be enabled again when two or more leaders join.\n")
+
 
     # Checks whether a voice channel is following Safe Ministry guidelines,
     # and deafens/undeafens the channel accordingly.
     async def manageChannel(self, voiceChannel):
         # Don't process the temp channel used for moving members
-        if voiceChannel is self.tempChannel:
+        if voiceChannel is None or voiceChannel.name.endswith(f"-temp-{voiceChannel.guild.id}"):
             return
         
         if self.validVoiceChannelState(voiceChannel):
             await self.setChannelDeafness(voiceChannel, False)
         else:
             await self.setChannelDeafness(voiceChannel, True)
+
 
     # Ensure safe ministry guidelines are followed in voice channels
     @commands.Cog.listener()
@@ -76,8 +85,12 @@ class SafeMinistry(commands.Cog, name='Safe Ministry'):
         elif before.channel is not None and after.channel is None:
             await self.manageChannel(before.channel)
         elif before.channel is not after.channel:
+            # Don't process when members are moved to a temp channel, causes loops
+            if after.channel.name.endswith(f"-temp-{after.channel.guild.id}"):
+                return
             await self.manageChannel(before.channel)
             await self.manageChannel(after.channel)
+
 
 def setup(bot):
     bot.add_cog(SafeMinistry(bot))
--- code/test.py
+++ code/test.py
@@ -1,9 +1,15 @@
-from extensions.modules.constants import Roles
-print(Roles.LEADER.value == "Leaders")
+# from extensions.modules.constants import Roles
+# print(Roles.LEADER.value == "Leaders")
 
 
-from datetime import datetime
-import pytz
-print(datetime.now(pytz.timezone('Australia/Sydney')).strftime("%H:%M:%S"))
+# from datetime import datetime
+# import pytz
+# print(datetime.now(pytz.timezone('Australia/Sydney')).strftime("%H:%M:%S"))
 
-print(type(5))
+# print(type(5))
+
+# if False:
+#     muteVideo = [1, 2, 3, 4]
+
+# if muteVideo:
+#     print("wow!")
