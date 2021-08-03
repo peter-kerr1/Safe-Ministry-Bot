3d3637695e72747f54dd2d311145c062e181b10c
Peter Kerr <30363159+peter-kerr1@users.noreply.github.com>
2021-08-03 01:35:56 +0000

    Upgrade repl.it for always-on capability

--- code/bot.py
+++ code/bot.py
@@ -1,11 +1,8 @@
 import os
 import logging
 import discord
-from dotenv import load_dotenv
 
 from discord.ext import commands
-
-from keep_awake import keep_awake
 
 # For logging events/errors
 logger = logging.getLogger('discord')
@@ -24,12 +21,8 @@ for filename in os.listdir('./extensions'):
     if filename.endswith('.py'):
         bot.load_extension(f"extensions.{filename[:-3]}")
 
-# Load the bot password (TOKEN) as an environment variable from a '.env' file in the same directory
-load_dotenv()
+# Load the bot password (TOKEN) as an environment variable
 TOKEN = os.getenv('BOT_TOKEN')
-
-# Keep the program running even when repl.it is closed
-keep_awake()
 
 # Start the bot
 bot.run(TOKEN)
